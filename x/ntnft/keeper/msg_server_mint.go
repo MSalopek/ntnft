8c586ae784f97fb33cee425b0de60e22e58a7017
Matija Salopek <matija.salopek994@gmail.com>
2022-08-14 20:58:04 +0200

    refactor mint message response

--- x/ntnft/keeper/msg_server_mint.go
+++ x/ntnft/keeper/msg_server_mint.go
@@ -3,15 +3,20 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"nt-nft/x/ntnft/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) Mint(goCtx context.Context, msg *types.MsgMint) (*types.MsgMintResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	cls, found := k.GetClass(ctx, msg.ClassId)
+
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "ClassID does not exist")
+	}
 
 	return &types.MsgMintResponse{}, nil
 }
