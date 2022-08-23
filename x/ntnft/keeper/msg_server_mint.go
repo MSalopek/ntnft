6f08b825ae8be480e867460f05944585404b5adb
Matija Salopek <matija.salopek994@gmail.com>
2022-08-23 22:21:55 +0200

    ntnft: refactor - move mint from msgServer to keeper

--- x/ntnft/keeper/msg_server_mint.go
+++ x/ntnft/keeper/msg_server_mint.go
@@ -2,9 +2,6 @@ package keeper
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"time"
 
 	"nt-nft/x/ntnft/types"
 
@@ -17,67 +14,17 @@ import (
 func (k msgServer) Mint(goCtx context.Context, msg *types.MsgMint) (*types.MsgMintResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	class, found := k.GetClass(ctx, msg.ClassId)
-
-	if !found {
-		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "ClassID does not exist: %s", msg.ClassId)
+	if msg == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "unprocessable messaage")
 	}
 
-	// TODO: should panic if this cannot be parsed
-	price, _ := sdk.ParseCoinsNormalized(class.Price)
-	creator, _ := sdk.AccAddressFromBech32(class.Creator)
-	requester, _ := sdk.AccAddressFromBech32(msg.Creator)
-
-	spendable := k.bankKeeper.SpendableCoins(ctx, requester)
-
-	if price.IsAllGT(spendable) {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Insufficient funds for minting token")
+	token, err := k.Keeper.MintToken(ctx, msg.ClassId, msg.Creator)
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "could not mint token")
 	}
 
-	// send to creator account
-	// NOTE: check creator account is module account
-	if err := k.bankKeeper.SendCoins(ctx, requester, creator, price); err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic,
-			fmt.Sprintf("unexpected error sending funds %s", err))
-	}
-
-	m := map[string]int64{
-		"ts": time.Now().UnixNano(),
-	}
-	meta, _ := json.Marshal(m)
-	token := types.NtNft{
-		// setting creator address index/Id
-		// avoids checking if creator owns token of class
-		// NOTE: this is bad, better to use counter for each class
-		Index:   msg.Creator,
-		ClassId: class.Index,
-		Owner:   msg.Creator,
-		// write timestamp in metadata
-		Data: string(meta),
-	}
-
-	// get all owner nfts and add newly minted
-	//
-	// NOTE: unsure about the write semantics here...
-	// maybe I should copy the entire owner, append to copy and
-	// set the copy as the new value
-	owner, _ := k.GetOwner(ctx, msg.Creator)
-	ownerColl := types.OwnerCollection{
-		ClassId: msg.ClassId,
-		Token:   &token,
-	}
-	owner.Collection = append(owner.Collection, &ownerColl)
-
-	class.Tokens = append(class.Tokens, &token)
-
-	// add new token
-	k.SetNtNft(ctx, token)
-	// add new token to owner collection
-	k.SetOwner(ctx, owner)
-	// add new token to class collection
-	k.SetClass(ctx, class)
 	return &types.MsgMintResponse{
-		ClassId: class.Index,
+		ClassId: token.ClassId,
 		TokenId: token.Index,
 		Owner:   token.Owner,
 	}, nil
--- x/ntnft/keeper/nt_nft.go
+++ x/ntnft/keeper/nt_nft.go
@@ -5,7 +5,71 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
+
+// MintToken mints a new NtNFT with classId and assigns ownership to createAddr.
+// Each owner can own at most one instance of a certain class.
+// An owner can attempt to mint another token of the same class
+// but a new token will not be issued. The functions will run normally
+// and the minting price defined on the Class will be deducted from the owner's acocunt.
+func (k Keeper) MintToken(ctx sdk.Context, classId, createAddr string) (types.NtNft, error) {
+
+	class, found := k.GetClass(ctx, classId)
+	if !found {
+		return types.NtNft{}, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "ClassId does not exist: %s", classId)
+	}
+
+	// something is seriously wrong if any of these return error
+	price, err := sdk.ParseCoinsNormalized(class.Price)
+	if err != nil {
+		panic(err)
+	}
+
+	classOwner, err := sdk.AccAddressFromBech32(class.Creator)
+	if err != nil {
+		panic(err)
+	}
+	requester, err := sdk.AccAddressFromBech32(createAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	spendable := k.bankKeeper.SpendableCoins(ctx, requester)
+
+	if price.IsAllGT(spendable) {
+		return types.NtNft{}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Insufficient funds for minting token")
+	}
+
+	// send to class classOwner account
+	if err := k.bankKeeper.SendCoins(ctx, requester, classOwner, price); err != nil {
+		return types.NtNft{}, sdkerrors.Wrap(err, "error sending funds")
+	}
+
+	token := types.NtNft{
+		// setting creator address index/Id
+		// avoids checking if creator owns token of class
+		// NOTE: it would be better to use a counter for each class
+		Index:   requester.String(),
+		ClassId: class.Index,
+		Owner:   requester.String(),
+	}
+
+	owner, _ := k.GetOwner(ctx, requester.String())
+	ownerCollection := types.OwnerCollection{
+		ClassId: class.Index,
+		Token:   &token,
+	}
+	owner.Collection = append(owner.Collection, &ownerCollection)
+
+	class.Tokens = append(class.Tokens, &token)
+
+	k.SetNtNft(ctx, token)
+	k.SetOwner(ctx, owner)
+	k.SetClass(ctx, class)
+
+	return token, nil
+}
 
 // SetNtNft set a specific ntNft in the store from its index
 func (k Keeper) SetNtNft(ctx sdk.Context, ntNft types.NtNft) {
